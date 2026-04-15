c9d3d1a0432a59de4e08f8e50149ab7423b75996
Omoshalewa Mercy <omoshalewamercy@gmail.com>
2026-04-15 21:47:43 +0100

    Form validation for specific input was updated. Logos were added on all pages.

--- JS/external.js
+++ JS/external.js
@@ -19,31 +19,73 @@ function filterMenu(category) {
 document.addEventListener("DOMContentLoaded", function () {
   const form = document.getElementById("reservationForm");
 
+  // gets all error message elements
+  const nameError = document.getElementById("name-error");
+  const emailError = document.getElementById("email-error");
+  const phoneError = document.getElementById("phone-error");
+  const guestsError = document.getElementById("guest-error");
+  const dateError = document.getElementById("date-error");
+  const timeError = document.getElementById("time-error");
+  const messageError = document.getElementById("message-error");
+
+ 
+
   if (form) {
     form.addEventListener("submit", function (e) {
       e.preventDefault();
-
+      
+      // get form values and trim whitespace
       let name = document.getElementById("name").value.trim();
       let email = document.getElementById("email").value.trim();
       let phone = document.getElementById("phone").value.trim();
       let guests = document.getElementById("guests").value;
 
-      if (name === "" || email === "" || phone === "" || guests === "") {
-        alert("Please fill in all required fields.");
-        return;
+      let date = document.getElementById("date");
+      let dateValue= document.getElementById("date").value;
+      let today = new Date().toISOString().split('T')[0];
+      date.setAttribute('min', today);
+
+      let time = document.getElementById("time").value;
+      let messages = document.getElementById("message").value;
+
+
+      // reset all error messages to hidden
+      nameError.setAttribute('style', 'display: block')
+      emailError.setAttribute('style', 'display: block')
+      phoneError.setAttribute('style', 'display: block')
+      guestsError.setAttribute('style', 'display: block')
+      dateError.setAttribute('style', 'display: block')
+      timeError.setAttribute('style', 'display: block')
+      
+      
+      // validation checks
+      if (name !== "") {
+        nameError.setAttribute('style', 'display: none')
       }
 
-      if (!email.includes("@")) {
-		  alert("Please enter a valid email.");
-		  return;
+      if (email.includes("@")) {
+        emailError.setAttribute('style', 'display: none')
       }
-	  
-	  if (!/^[0-9]+$/.test(phone)) {
-		  alert("Phone number should only be digits");
-		  return;
-	  }
 
-      alert("Reservation submitted successfully!");
+      if (/^[0-9]+$/.test(phone)) {
+        phoneError.setAttribute('style', 'display: none')
+      }
+
+      if ((guests !== "") && (guests >= 1)) {
+        guestsError.setAttribute('style', 'display: none')
+      }
+      if ((dateValue !== "") && (dateValue >= today)) {
+        dateError.setAttribute('style', 'display: none')
+      }
+      if (time !== "") {
+        timeError.setAttribute('style', 'display: none')
+      }
+
+      if(!name|| !email|| !phone || !guests || guests < 1 || !date || dateValue < today ||!time){
+        return
+      }else{
+        alert("Reservation submitted successfully!");
+      }
       form.reset();
     });
   }
